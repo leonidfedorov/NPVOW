05866863d0a80bd679068c2599286137f8519d6d
Leonid Fedorov <leonidf87@gmail.com>
2014-08-18 20:19:52 +0200

    added plotting routine with mean and std computations

--- LFAP/exp1/loadsubstat.m
+++ LFAP/exp1/loadsubstat.m
@@ -1,5 +1,5 @@
 function[towa,away]=loadsubstat(subname)
-close all;
+%close all;
 %subname = '';
 
 
@@ -24,25 +24,25 @@ towa = [towaComplete(1:17,1:2),reshape(towaComplete(:,3),[17 size(towaComplete,1
 away = [awayComplete(1:17,1:2),reshape(awayComplete(:,3),[17 size(awayComplete,1)/17])];
 
 
-towamean = mean(towa(:,3:17),2);
-towastd = std(towa(:,3:17)')/4;
-errorbar(towa(:,1),towamean,towastd,'r');
-xlim([-pi/4 5*pi/4]);
-ylim([-0.2 1.2]);
-grid on;
-title({'walking towards', 'increase in X axis means light source moving up', ...
-    'increase in Y axis means being more correct'});
-
-awaymean = mean(away(:,3:17),2);
-awaystd = std(away(:,3:17)')/4;
-figure;errorbar(away(:,1),awaymean,awaystd,'b');
-xlim([3*pi/4 9*pi/4]);
-ylim([-0.2 1.2]);
-grid on;
-title({'walking away', 'increase in X axis means light source moving bottom', ...
-    'increase in Y axis means being less correct'});
-
-[p_towa,tab_towa, stats_towa] = anova1(towa(:,3:17)')
-[p_away,tab_away, stats_away] = anova1(away(:,3:17)')
+% towamean = mean(towa(:,3:17),2);
+% towastd = std(towa(:,3:17)')/4;
+% errorbar(towa(:,1),towamean,towastd,'r');
+% xlim([-pi/4 5*pi/4]);
+% ylim([-0.2 1.2]);
+% grid on;
+% title({'walking towards', 'increase in X axis means light source moving up', ...
+%     'increase in Y axis means being more correct'});
+% 
+% awaymean = mean(away(:,3:17),2);
+% awaystd = std(away(:,3:17)')/4;
+% figure;errorbar(away(:,1),awaymean,awaystd,'b');
+% xlim([3*pi/4 9*pi/4]);
+% ylim([-0.2 1.2]);
+% grid on;
+% title({'walking away', 'increase in X axis means light source moving bottom', ...
+%     'increase in Y axis means being less correct'});
+% 
+% [p_towa,tab_towa, stats_towa] = anova1(towa(:,3:17)')
+% [p_away,tab_away, stats_away] = anova1(away(:,3:17)')
 
 return
