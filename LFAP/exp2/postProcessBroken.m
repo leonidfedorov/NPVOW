e9af53e144ce5e10257599487092601ded994b6f
Leonid Fedorov <leonidf87@gmail.com>
2015-02-09 17:17:55 +0100

    1) unify AhmetD and HannesS recording, that was split in chunks 2) just extend the list of colors to 20 for plotting 3) add all new relevant response files to BG0.8/results

--- LFAP/exp2/postProcessBroken.m
+++ LFAP/exp2/postProcessBroken.m
@@ -65,30 +65,74 @@ path = 'C:\Users\LocalAdminLeo\Documents\MATLAB\NPVOW\LFAP\exp2\results\';
 % % % % save(strcat(path,'mariak\mariak_resp',num2str(x),'.mat'),'resp');
 
 
+% % % % %%%%%%%%
+% % % % path = 'C:\Users\LocalAdminLeo\Documents\MATLAB\NPVOW\LFAP\exp2\results\';
+% % % % 
+% % % % te1 = load(strcat(path, 'verenat\VT_resp274.mat'));
+% % % % resp1 = te1.('resp');
+% % % % resp1 = resp1(resp1(:,5)>1 & resp1(:,5)<8,:);
+% % % % resp1(:,5) = resp1(:,5) - 1;
+% % % % 
+% % % % te2 = load(strcat(path, 'verenat\VT_resp473.mat'));
+% % % % resp2 = te2.('resp');
+% % % % resp2 = resp2(resp2(:,5)<14,:);
+% % % % resp2(:,5) = resp2(:,5) + 6;
+% % % % 
+% % % % te3 = load(strcat(path, 'verenat\VT2_resp366.mat'));
+% % % % resp3 = te3.('resp');
+% % % % resp3 = resp3(resp3(:,5)<2,:);
+% % % % resp3(:,5) = resp3(:,5) + 19;
+% % % % 
+% % % % 
+% % % % resp = cat(1,resp1,resp2,resp3);
+% % % % 
+% % % % [x,y] = size(resp)
+% % % % 
+% % % % save(strcat(path,'verenat\verenat_resp',num2str(x),'.mat'),'resp');
+
+
+% % % % %%%%%%%%
+% % % % path = 'C:\Users\LocalAdminLeo\Documents\MATLAB\NPVOW\LFAP\exp2\results\';
+% % % % 
+% % % % te1 = load(strcat(path, 'hanness\hs_resp648.mat'));
+% % % % resp1 = te1.('resp');
+% % % % resp1 = resp1(resp1(:,5)>1 & resp1(:,5)<19,:);
+% % % % resp1(:,5) = resp1(:,5) - 1;
+% % % % 
+% % % % te2 = load(strcat(path, 'hanness\hs2_resp108.mat'));
+% % % % resp2 = te2.('resp');
+% % % % resp2 = resp2(resp2(:,5)<inf,:);
+% % % % resp2(:,5) = resp2(:,5) + 17;
+% % % % 
+% % % % resp = cat(1,resp1,resp2);
+% % % % 
+% % % % [x,y] = size(resp)
+% % % % 
+% % % % save(strcat(path,'hanness\hanness_resp',num2str(x),'.mat'),'resp');
+
+
 %%%%%%%%
 path = 'C:\Users\LocalAdminLeo\Documents\MATLAB\NPVOW\LFAP\exp2\results\';
 
-te1 = load(strcat(path, 'verenat\VT_resp274.mat'));
+te1 = load(strcat(path, 'hanness\hs_resp648.mat'));
+te1 = load(strcat(path, 'ahmetd\AD_resp181.mat'));
 resp1 = te1.('resp');
-resp1 = resp1(resp1(:,5)>1 & resp1(:,5)<8,:);
+resp1 = resp1(resp1(:,5)>1 & resp1(:,5)<6,:);
 resp1(:,5) = resp1(:,5) - 1;
 
-te2 = load(strcat(path, 'verenat\VT_resp473.mat'));
+te2 = load(strcat(path, 'ahmetd\AD2_resp648.mat'));
 resp2 = te2.('resp');
-resp2 = resp2(resp2(:,5)<14,:);
-resp2(:,5) = resp2(:,5) + 6;
+%AhmetD was recorded in clean 4 + clean 18 rounds, so we only take first
+%clean 4 in lines above, and then the next clean 16 ONLY, dropping the last 2 in the line below
+resp2 = resp2(resp2(:,5)<17,:);
+resp2(:,5) = resp2(:,5) + 4;
 
-te3 = load(strcat(path, 'verenat\VT2_resp366.mat'));
-resp3 = te3.('resp');
-resp3 = resp3(resp3(:,5)<2,:);
-resp3(:,5) = resp3(:,5) + 19;
-
-
-resp = cat(1,resp1,resp2,resp3);
+resp = cat(1,resp1,resp2);
 
 [x,y] = size(resp)
 
-save(strcat(path,'verenat\verenat_resp',num2str(x),'.mat'),'resp');
+save(strcat(path,'ahmetd\ahmetd_resp',num2str(x),'.mat'),'resp');
+
 
 
 end
