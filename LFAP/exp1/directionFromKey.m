08c1c92effa58b0ed422f817389c32d2c8ca3394
leonidfedorov <leonidf87@gmail.com>
2015-11-16 18:05:41 +0100

    untracking exp results

--- LFAP/exp1/directionFromKey.m
+++
@@ -1,9 +0,0 @@
-function [direction] = directionFromKey(key)
-    if strcmp(key,'up')
-        direction = 0;
-    elseif strcmp(key,'down')
-        direction = 1;
-    else
-        direction = NaN;
-    end
-    return
--- LFAP/exp2/directionFromFilename.m
+++
@@ -1,9 +0,0 @@
-function [direction] = directionFromFilename(filename)
-    pattern = '^[a-z]{4}'; %match first four alphanumerical symbols within a filename
-    strdirection = regexp(filename, pattern, 'match');
-    if strcmp(strdirection,'towa')
-        direction = 1;
-    elseif strcmp(strdirection,'away')
-        direction = 0;
-    end
-    return
