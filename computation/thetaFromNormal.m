3ce8da03c12c2b0510eafbed3619ad478a6b85e8
leonidfedorov <leonidf87@gmail.com>
2016-02-08 22:28:40 +0100

    added Tikhonov rigularization with a single parameter alpha

--- computation/thetaFromNormal.m
+++ computation/thetaFromNormal.m
@@ -1,11 +1,11 @@
-function [theta]= thetaFromNormal(X, y)
+function [theta]= thetaFromNormal(X, y, alpha)
     feat = @(A) cat(2, ones(size(A, 1), 1), A);
-    matrsol = @(A) pinv(A'*A)*A';
+    matrsol = @(A, c) pinv(A'*A)*A' + c*eye(size(A, 1));
     if (size(feat(X), 1) == size(y, 1)) && (size(y, 2) == 1)
-        theta = matrsol(feat(X))*y;
+        theta = matrsol(feat(X), alpha)*y;
     else
         display('Check input sizes. Recall that y should be a column vector.');
-        display(num2str(size(feat(X)')),'Size of X-transpose:');
+        display(num2str(size(feat(X)')),'Size of X-transposed:');
         display(num2str(size(y)),'Size of y:');
         theta = inf(size(feat(X)'));
     end
