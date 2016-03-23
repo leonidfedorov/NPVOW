c701dbed0cf60843910d6a2183c4a0859a098b59
leonidfedorov <leonidf87@gmail.com>
2016-03-23 01:07:39 +0100

    small bugfix

--- runField.m
+++ runField.m
@@ -4,8 +4,7 @@ for ad = adrange,
     for noise = nrange,
         tic
         disp(sprintf('Will simulate for adaptation: %0.5f ; noise level: %0.5f', ad, noise));
-        display(noise)
-        UST = travcomp_ad(adrange, nrange);
+        UST = travcomp_ad2(ad, noise);
         save(fullfile(folder, strcat(ad, '_', noise, '_UST.mat')), 'UST');
         display('Saved simulation result.');
         toc
