c2c877212d8f6303be3924b847ecfbc9d2166db3
leonidfedorov <leonidf87@gmail.com>
2016-03-27 21:30:10 +0200

    update to only compute uncomputed activities

--- dynamics/analyseField.m
+++ dynamics/analyseField.m
@@ -6,21 +6,23 @@ if isstr(suffix),
 
     for simind = 1:numel(simlist),
         file = simlist(simind);
-        tic
-        disp(sprintf('Will load field activity from: %s ; sized %0.5f Mb', file.name, file.bytes/(1024^2)));
-        sim = load(fullfile(folder,file.name));
+	    if exist(fullfile(folder, getFieldFilename(file.name, 'fieldact')), 'file') ~= 2,
+ 	        tic
+            disp(sprintf('Will load field activity from: %s ; sized %0.5f Mb', file.name, file.bytes/(1024^2)));
+     	    sim = load(fullfile(folder,file.name));
 
-        display('Loaded field activity, will analyze percept times.');
-        toc
+       	    display('Loaded field activity, will analyze percept times.');
+            toc
 
-        fieldact.ptimes = actanal2(sim.(suffix).('UST'));
-        fieldact.adaptation = sim.(suffix).('adaptation');
-        fieldact.noise = sim.(suffix).('noise');
+            fieldact.ptimes = actanal2(sim.(suffix).('UST'));
+            fieldact.adaptation = sim.(suffix).('adaptation');
+            fieldact.noise = sim.(suffix).('noise');
 
-        save(fullfile(folder, strcat(num2str(fieldact.adaptation), '_', num2str(fieldact.noise), '_fieldact.mat')), 'fieldact');
+            save(fullfile(folder, strcat(num2str(fieldact.adaptation), '_', num2str(fieldact.noise), '_fieldact.mat')), 'fieldact');
 
-        display('Saved percept times.');
-        toc
+            display('Saved percept times.');
+            toc
+	    end
     end
 
     success = 1;
