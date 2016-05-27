eaf4ae580c45b5844f02be7298eeb2ae23b71116
leonidfedorov <leonidf87@gmail.com>
2016-05-27 17:33:25 +0200

    counting the number of consecutive periods

--- dynamics/poincareSection.m
+++ dynamics/poincareSection.m
@@ -12,26 +12,52 @@ clear te;
 
 
 
-for ind = 2:50:36000
-    section((ind - mod(ind,50))/50 + 1, :) = [activity(1, 1:25, ind), activity(26, 26:50, ind)];
+for ind = 2:50:360000
+    section((ind - mod(ind, 50)) / 50 + 1, :) = [activity(1, 1:25, ind), activity(26, 26:50, ind)];
 end
 
 figure;
-map(:,:,:) = activity(:,:,2:50:36000);
-surf(mean(map, 3));
-
-figure;
-subplot(1, 4, 1); waterfall(section); view([-40 30]); title({'Iteration of neural activity with time period coinciding'; 'with the input period; -40, 30 view'})
-subplot(1, 4, 2); waterfall(section); view([-20 65]); title('Same as left; -20, 65 view')
+subplot(2, 3, 1);
+map(:, :, :) = activity(:, :, 2:50:36000);
+surf(mean(map, 3)); title('Mean of Poincare Section');
 
 
-subplot(1, 4, 3); 
-plot(mean(section,1)); title('Mean of iterations, matches the input distribution.')
+subplot(2, 3, 2); waterfall(section); view([-40 30]); title({'Spatial Section of Poincare Section'; 'at the supposed maximum position.'})
+subplot(2, 3, 3); waterfall(section); view([-60 90]); title('Same as left; -60, 90 view')
 
-subplot(1, 4, 4);
+
+subplot(2, 3, 4); 
+errorbar(mean(section, 1), std(section, 1)); title('Mean and Standard Error of iterations, matches the input distribution.')
+
+subplot(2, 3, 5);
 for i = 1:720, plot(section(i, :)); hold on; end
 title('Iterations plotted on top of each other.')
 
+ptCount = Inf(size(section, 1), 2);
+leftCount = [0];
+rightCount = [0];
+for i = 2:size(section,1),
+    if and(max(section(i, 1:25) >= 0.1), max(section(i, 26:50) < 0.1)),
+        ptCount(i, 1) = 1;
+        if ptCount(i - 1, 1) == Inf,
+            leftCount = [leftCount 1];
+        else
+            leftCount(end) = leftCount(end) + 1;
+        end
+    elseif and(max(section(i, 1:25) < 0.1), max(section(i, 26:50) >= 0.1)),
+        ptCount(i, 2) = 1;
+        if ptCount(i - 1, 2) == Inf,
+            rightCount = [rightCount 1];
+        else
+            rightCount(end) = rightCount(end) + 1;        
+        end
+    else
+        ptCount(i, :) = [1 1];
+    end
+end
+
+subplot(2,3,6)
+qqplot(leftCount, makedist( 'exponential'));
 % section = 0;
 
 return
