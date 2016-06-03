39277d597b963b9f4f65d0a7f71b352cf51b6137
leonidfedorov <leonidf87@gmail.com>
2016-06-03 22:00:07 +0200

    extensive comments

--- stimuligenerator/randomPatch.m
+++ stimuligenerator/randomPatch.m
@@ -1,4 +1,54 @@
 function [outpath] = randomPatch(in1, in2, out, radius, phnum1, phnum2, blend)
+%randomPatch(in1, in2, out, radius, phnum1, phnum2, blend)
+%
+%Generates a patched mix between two videos.
+%
+%Considering one video, it generates a specified number of "pinholes", of 
+%specified size at random positions for every frame. Every pixel outside 
+%the "pinholes" is set to black.
+%
+%Given the pixels from inside the "pinholes" from both videos, we compose
+%a new frame by applying them on top of each other. In case the "pinholes"
+%overlap, the pixels are taken from the second video(specified by 'in2').
+%
+%One can fix the total number of "pinholes" as phnum1 + phnum2, and
+%then change the ratio between phnum1 and phnum2, thus biasing 
+%the resulting video towards 'in1' or 'in2'
+%
+%For example, setting phnum1 = 1000, phnum2 = 0, will have 1000 "pinholes"
+%in the resulting video('out'), but all pixels will be taken from 'in1'
+%Another example, setting phnum1 = 700, phnum2 = 300 will have 700 
+%"pinholes" from 'in1' and 300 "pinholes" from 'in2'.
+%
+%In both cases we have 1000 "pinholes". Therefore we (coarsely)can think 
+%that 'in1' has probability phnum1/(phnum1+phnum2) and 'in2' has
+%probability phnum2/(phnum1+phnum2) to "show up".
+%
+%Then in the first example above this is, accordingly 
+%1000 / (1000 + 0) = 1.0 and 0 / (1000 + 0) = 0.0
+%and in the second example this is
+%700 / (700 + 300) = 0.7 and 300 / (700 + 300) = 0.3
+%
+%One should fix phnum1 + phnum2 and radius for a given experiment.
+%
+%Patch size can be controlled by the radius parameter.
+%
+%In the first pilot experiment we set radius=10.
+%To try help 'uncertain' participants we set radius=12.
+%In both cases we set phnum1 + phnum2 = 1000.
+%
+%Description of arguments:
+%in1 is a full path to the first AVI file
+%in2 is a full path to the second AVI file
+%out is a full path to the resulting AVI file
+%radius is the radius of the disks
+%phnum1 is the number of disks applied to the first AVI file
+%phnum2 is the number of disks applied to the second AVI file
+%blend is presently unused; in principle it is supposed to control the 
+%       involvement of first and second file in case the disks overlap.
+%
+%-LF 06.03.2016
+%
 
 close all;
 in1vid = VideoReader(in1)
