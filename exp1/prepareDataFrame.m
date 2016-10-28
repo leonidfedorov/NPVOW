a46ab8a918e29ced83b675cc75d6e3e76ff4a575
leonidfedorov <leonidf87@gmail.com>
2016-10-28 17:05:57 +0200

    change the response encoding

--- exp1/prepareDataFrame.m
+++ exp1/prepareDataFrame.m
@@ -19,17 +19,33 @@ for i = 1:numel(reslist),
         prev_ans = resp( max( find(resp(:, 6) < ans_num)), 3);
         
         if numel(prev_ans) == 0,
-            prev_ans = 2.718281828;
+            prev_ans = 2;
         end
         if resp(j,2) == 1,
-            angle = rad2deg(resp(j,  1));
+            angle = resp(j,  1);
         elseif resp(j, 2) == 0,
-            angle = 360 - rad2deg(resp(j,  1));
+            angle = 2*pi - resp(j,  1);
         end
         datarow = [i angle resp(j, 2:3) prev_ans];
         dataframe = cat(1, dataframe, datarow);
     end
 end
+%previously the walking direction response were encoded as 0 if its away
+%and 1 if its towards;
+%below we re-encode the responses as 1 if the walking direction is veridical,
+%and as 0 if its not
+counter = 0;
+for k = 1 : size(dataframe,1),
+    if dataframe(k, 3) == 0, % if the walking direction was away
+        dataframe(k, 4) = 1 - dataframe(k, 4); %flip the bit in the original response
+        if dataframe(k, 5) == 2, %if the previous response was empty just count how many times we encounter that to double-check
+            counter = 1 + counter; 
+        else %if the previous response was not empty, flip the bit too
+            dataframe(k, 5) = 1 - dataframe(k, 5);
+        end
+    end
+end
+% counter
 filepath = fullfile(folder, 'exp1_data.csv');
 csvwrite(filepath, dataframe);
 
