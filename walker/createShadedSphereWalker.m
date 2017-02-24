0b0098cdab14a0be05cc7d114981fc17acdd4d5b
leonidfedorov <leonidf87@gmail.com>
2017-02-24 19:54:48 +0100

    refactored to work on a bunch of avis in a directory

--- walker/createShadedSphereWalker.m
+++ walker/createShadedSphereWalker.m
@@ -1,12 +1,25 @@
 function createShadedSphereWalker(folder)
 narginchk(1,1)
 
+
+nameAVIs = dir(char(fullfile(folder,'*.avi')));
+
+for ii = 1 : numel(nameAVIs),
+    aviname = nameAVIs(ii).name;
+    partFoldername = aviname(1 : end-4);
+    mkdir(fullfile(strcat(folder), partFoldername));
+    movefile(fullfile(folder, aviname), fullfile(folder, partFoldername, aviname));
+
+end
+
+
 d = dir(folder)
 isub = [d(:).isdir];
 nameFolds = {d(isub).name}';
 nameFolds(ismember(nameFolds,{'.', '..'})) = [];
 bg = 0.01;
 fg = 0.98;
+
 
 for kk = 1 : numel(nameFolds),
     partFoldername = char(nameFolds(kk));
@@ -29,6 +42,7 @@ for kk = 1 : numel(nameFolds),
     maskim_path = fullfile(folder, partFoldername, partFilename_bw(1 : end - 4));
     grayim_path = fullfile(folder, partFoldername, partFilename(1 : end - 4));
     
+    tic
     pngListing = dir(fullfile(maskim_path, '*.png'));
     for ii = 1 : numel(pngListing),
         maskim = fullfile(maskim_path, pngListing(ii).name);
@@ -36,5 +50,6 @@ for kk = 1 : numel(nameFolds),
         interpim = fullfile(interpim_path, pngListing(ii).name);
         interpolate2Spheres(maskim, grayim, interpim);
     end
+    toc
 end
 
