bd54c9b76a7a10351c5180a56881930269c0eedd
leonidfedorov <leonidf87@gmail.com>
2017-03-03 19:46:45 +0100

    simply wrong

--- computation/isFloatInVec.m
+++
@@ -1,8 +0,0 @@
-function [answer] = isFloatInVec(x, vector, prec)
-%checks if a floating point number is in the floating point vector
-%with a given precision(doesn't work at high precision that pushes the MATLAB limits)
-
-
-answer = any( abs(vector - x) < prec );
-
-return
